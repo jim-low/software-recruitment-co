c0caab0b12f2f7d6cb76091423302b38c9b911b3
jim-low <robil12380@gmail.com>
2025-07-29 23:37:13 +0800

    feat: apologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Footer from './components/footer';
 import Header from './components/header';
@@ -6,6 +7,28 @@ import LatestJobs from './components/latest-jobs';
 import WhoWeWorkWith from './components/who-we-work-with';
 
 function App() {
+  const [showDisappointmentMessage, setShowDisappointmentMessage] = useState(false);
+
+  useEffect(() => {
+    const handleOnResize = () => {
+      setShowDisappointmentMessage(document.documentElement.clientWidth <= 600);
+    };
+
+    handleOnResize();
+    window.addEventListener('resize', handleOnResize);
+    return () => {
+      window.removeEventListener('resize', handleOnResize);
+    }
+  }, []);
+
+  if (showDisappointmentMessage) {
+    return (
+      <p className='text-center w-[400px] m-auto'>
+        Unfortunately there was not enough time to implement mobile responsiveness :(
+      </p>
+    );
+  }
+
   return (
     <div className='app'>
       <Header />
